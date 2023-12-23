934e9b8b5d1c11afa91a810a9a68e8220847b13e
s0j011f <s0j011f@homeoffice.wal-mart.com>
2023-12-23 10:33:07 +0530

    Basic Setup

---
+++ src/main/java/com/mywealthhub/MyWealthHubApp.java
@@ -0,0 +1,11 @@
+package com.mywealthhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWealthHubApp {
+	public static void main(String[] args) {
+		SpringApplication.run(MyWealthHubApp.class, args);
+	}
+}
